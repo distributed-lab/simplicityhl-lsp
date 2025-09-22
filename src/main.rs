e6a56a57838d1da49bbc6e9abda01e4d9a36a6fe
gerau <cholkegn@gmail.com>
2025-09-22 14:37:30 +0300

    added semantic highlighting with SimplicityHL tree sitter

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::fmt::Write;
+
 use dashmap::DashMap;
 use ropey::Rope;
 use serde_json::Value;
@@ -6,25 +8,50 @@ use tower_lsp_server::jsonrpc::Result;
 use tower_lsp_server::lsp_types::*;
 use tower_lsp_server::{Client, LanguageServer, LspService, Server};
 
+use tree_sitter::{self, StreamingIterator};
+use tree_sitter_simfony;
+
 use simplicityhl::{
-    CompiledProgram, ast,
+    ast,
     error::{RichError, Span, WithFile},
     parse,
     parse::ParseFromStr,
 };
 
-mod symbol_table;
-
 #[derive(Debug)]
 struct Backend {
     client: Client,
     document_map: DashMap<String, Rope>,
+    token_legend: Vec<SemanticTokenType>,
+    token_map: DashMap<String, u32>,
 }
 
 struct TextDocumentItem<'a> {
     uri: Uri,
     text: &'a str,
     version: Option<i32>,
+}
+
+fn build_token_map(legend: &[SemanticTokenType]) -> DashMap<String, u32> {
+    legend
+        .iter()
+        .enumerate()
+        .map(|(i, t)| (t.as_str().to_string(), i as u32))
+        .collect()
+}
+
+fn get_token_capabilities(
+    token_types: &Vec<SemanticTokenType>,
+) -> SemanticTokensServerCapabilities {
+    SemanticTokensServerCapabilities::SemanticTokensOptions(SemanticTokensOptions {
+        legend: SemanticTokensLegend {
+            token_types: token_types.clone(),
+            token_modifiers: vec![],
+        },
+        full: Some(SemanticTokensFullOptions::Bool(true)),
+        range: None,
+        ..Default::default()
+    })
 }
 
 impl LanguageServer for Backend {
@@ -60,6 +87,7 @@ impl LanguageServer for Backend {
                     }),
                     file_operations: None,
                 }),
+                semantic_tokens_provider: Some(get_token_capabilities(&self.token_legend)),
                 ..ServerCapabilities::default()
             },
             ..Default::default()
@@ -155,6 +183,23 @@ impl LanguageServer for Backend {
             CompletionItem::new_simple("Bye".to_string(), "More detail".to_string()),
         ])))
     }
+
+    async fn semantic_tokens_full(
+        &self,
+        params: SemanticTokensParams,
+    ) -> Result<Option<SemanticTokensResult>> {
+        let uri = params.text_document.uri;
+        let text = match self.document_map.get(uri.as_str()) {
+            Some(rope) => rope.to_string(),
+            None => "".to_string(),
+        };
+        let tokens = self.highlight_with_treesitter(&text).await;
+
+        Ok(Some(SemanticTokensResult::Tokens(SemanticTokens {
+            result_id: None,
+            data: tokens,
+        })))
+    }
 }
 
 fn span_to_positions(span: &Span) -> (Position, Position) {
@@ -171,6 +216,64 @@ fn span_to_positions(span: &Span) -> (Position, Position) {
 }
 
 impl Backend {
+    async fn highlight_with_treesitter(&self, code: &str) -> Vec<SemanticToken> {
+        let mut parser = tree_sitter::Parser::new();
+        let language = tree_sitter_simfony::LANGUAGE;
+
+        parser
+            .set_language(&language.into())
+            .expect("Error loading Simfony parser");
+        let tree = parser.parse(code, None).unwrap();
+
+        let query = tree_sitter::Query::new(&language.into(), include_str!("highlights.scm"))
+            .expect("file should open and be valid");
+        let mut cursor = tree_sitter::QueryCursor::new();
+        let mut tokens = Vec::new();
+
+        let (mut last_line, mut last_col) = (0, 0);
+
+        cursor
+            .matches(&query, tree.root_node(), code.as_bytes())
+            .for_each(|m| {
+                for cap in m.captures {
+                    let node = cap.node;
+                    let (line, col) = (
+                        node.start_position().row as u32,
+                        node.start_position().column as u32,
+                    );
+                    let (delta_line, delta_start) = if line == last_line {
+                        (0, col - last_col)
+                    } else {
+                        (line - last_line, col)
+                    };
+
+                    let length = node.end_byte() - node.start_byte();
+                    let kind = query.capture_names()[cap.index as usize];
+                    let token_type_index = self.token_map.get(kind);
+
+                    match token_type_index {
+                        Some(index) => {
+                            tokens.push(SemanticToken {
+                                delta_line: delta_line,
+                                delta_start: delta_start,
+                                length: length as u32,
+                                token_type: *index,
+                                token_modifiers_bitset: 0,
+                            });
+
+                            (last_line, last_col) = (line, col);
+                        }
+                        None => {}
+                    }
+                }
+            });
+        self.client
+            .log_message(MessageType::LOG, "Done with semantic highlight!")
+            .await;
+
+        tokens
+    }
+
     fn parse_program(text: &str) -> Option<RichError> {
         let parse_program = match parse::Program::parse_from_str(text) {
             Ok(p) => p,
@@ -178,7 +281,7 @@ impl Backend {
         };
 
         match ast::Program::analyze(&parse_program).with_file(text) {
-            Ok(ast) => None,
+            Ok(_ast) => None,
             Err(e) => Some(e),
         }
     }
@@ -225,9 +328,22 @@ impl Backend {
 async fn main() {
     let (stdin, stdout) = (tokio::io::stdin(), tokio::io::stdout());
 
+    let legend: Vec<SemanticTokenType> = vec![
+        "function".into(),
+        "variable".into(),
+        "keyword".into(),
+        "type".into(),
+        "parameter".into(),
+        "comment".into(),
+        "number".into(),
+        "operator".into(),
+    ];
+
     let (service, socket) = LspService::new(|client| Backend {
         client: client,
         document_map: DashMap::new(),
+        token_map: build_token_map(&legend),
+        token_legend: legend,
     });
     Server::new(stdin, stdout, socket).serve(service).await;
 }
